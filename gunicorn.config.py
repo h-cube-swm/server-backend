f5b39bb792cc2ec3936928a17a4544b14ee98dde
SeungWookHan <hswook12@me.com>
2021-07-12 18:59:16 +0000

    Fix gunicorn logging system

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -5,5 +5,5 @@ wsgi_app = 'src.wsgi:application'
 bind = '0.0.0.0:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = multiprocessing.cpu_count() * 2 + 1
-access_logfile = "/var/log/gunicorn_log/gunicorn-access.log"
-error_logfile = "/var/log/gunicorn_log/gunicorn-error.log"
+accesslog = "/var/log/gunicorn_log/gunicorn-access.log"
+errorlog = "/var/log/gunicorn_log/gunicorn-error.log"
