cc4ee14248de8d86589df6a3ff7f9b1f0a8335a8
SeungWookHan <hswook12@me.com>
2021-07-16 06:48:44 +0000

    Reset http responses

--- utils/responses.py
+++ utils/responses.py
@@ -7,18 +7,5 @@ no = {"success": False, "status": 400, "comment": "NO"}
 
 illegalArgument = {"success": False, "status": 400, "comment": "Illegal Argument"}
 
-# Users
-notAuthorized = {"success": False, "status": 400, "comment": "User Not Authorized"}
-
-invalidUID = {"success": False, "status": 400, "comment": "Invalid UID"}
-createUserSucceed = {"success": True, "status": 200, "comment": "create user succeed"}
-modifyUserSucceed = {"success": True, "status": 200, "comment": "modify user succeed"}
-deleteUserSucceed = {"success": True, "status": 200, "comment": "delete user succeed"}
-userAlreadyRegistered = {
-    "success": False,
-    "status": 403,
-    "comment": "User(UID) has already registered.",
-}
-noUser = {"success": False, "status": 401, "comment": "there's no matched user"}
-tokenRequired = {"success": False, "status": 400, "comment": "Token argument required"}
-invalidToken = {"success": False, "status": 400, "comment": "Invalid Token"}
+# Survey 관련
+invalidSurveyID = {"success": False, "status": 400, "comment": "Invalid Survey ID"}
