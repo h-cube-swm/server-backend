beac4be35c3b30029a10593d079c571aa1cf4602
SeungWookHan <hswook12@me.com>
2021-07-16 11:40:52 +0000

    Add noAPI responses

--- utils/responses.py
+++ utils/responses.py
@@ -1,6 +1,11 @@
 # JSON 응답 딕셔너리를 모아둔 코드
 APIOnly = {"success": True, "status": 200, "comment": "API Only"}
 postOnly = {"success": False, "status": 400, "comment": "only POST allowed"}
+noAPI = {
+    "success": False,
+    "status": 405,
+    "comment": "This HTTP Method Does Not Supported",
+}
 
 ok = {"success": True, "status": 200, "comment": "OK"}
 no = {"success": False, "status": 400, "comment": "NO"}
