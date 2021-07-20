2e9b901e2ee9a94fbd4ce69e1dd1f6873e4f61ef
SeungWookHan <hswook12@me.com>
2021-07-20 17:38:33 +0000

    Fix surveyAlreadyEnd response

--- utils/responses.py
+++ utils/responses.py
@@ -30,8 +30,8 @@ modifySurveySucceed = {
     "comment": "Modify Survey Succeed",
 }
 surveyAlreadyEnd = {
-    "success": False,
-    "status": 400,
+    "success": True,
+    "status": 200,
     "comment": "Survey Already Published or Closed",
 }
 
