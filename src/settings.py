af463c07384b2cd99ede1df261586496fbbabe88
SeungWookHan <hswook12@me.com>
2021-08-17 16:25:29 +0000

    Modify DATABASES Settings

--- src/settings.py
+++ src/settings.py
@@ -103,12 +103,8 @@ WSGI_APPLICATION = "src.wsgi.application"
 if DEBUG:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "development",
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "PORT": "5432",
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
 else:
