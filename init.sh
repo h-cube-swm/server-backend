867f0fbffd1a0fb97729739e0010da7e9504168f
unknownpgr <unknownpgr@gmail.com>
2021-08-21 05:00:15 +0000

    Remove superuser script

--- init.sh
+++ init.sh
@@ -1,12 +1,9 @@
+# Read environment variables from .env file
+
 python manage.py makemigrations
 python manage.py migrate
 
 python manage.py collectstatic --noinput
-python manage.py createsuperuserwithpassword \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --password $DJANGO_SUPERUSER_PASSWORD \
-        --email $DJANGO_SUPERUSER_EMAIL \
-        --preserve
 
 # gunicorn settings
 # 참고: https://docs.gunicorn.org/en/stable/settings.html
